c8158a21aa231cc8f020c1506fdf18a06e2f3f3a
cdiakite711 <141065282+cdiakite711@users.noreply.github.com>
2023-08-15 18:50:54 +0000

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
   # Do not use this in production. Sample purpose only.
   # Author: Upkar Lidder (IBM)
   # Additional Authors:
-  # <your GitHub username>
+  # <cdiakite711>
   # Input:
   # p, principal amount
   # t, time period in years
